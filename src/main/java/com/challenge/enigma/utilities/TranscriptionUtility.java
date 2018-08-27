03fbe0ebfd4c79d5b59d76f4b4f27a6ffb1b0bf4
jexiaprogrammingchallenge <42712417+jexiaprogrammingchallenge@users.noreply.github.com>
2018-08-27 19:47:02 +0300

    enigma application can call transcriber now

---
+++ src/main/java/com/challenge/enigma/utilities/TranscriptionUtility.java
@@ -0,0 +1,28 @@
+package com.challenge.enigma.utilities;
+
+import com.challenge.enigma.symbolset.SymbolSet;
+import com.challenge.enigma.exceptions.SymbolSetNotExistException;
+import com.challenge.enigma.exceptions.IllegalSymbolException;
+import java.util.List;
+
+public class TranscriptionUtility {
+
+    private SymbolSet symbolSet;
+
+    public void setSymbolSet(SymbolSet symbolSet) {
+        this.symbolSet = symbolSet;
+    }
+
+    public SymbolSet getSymbolSet() {
+        return symbolSet;
+    }
+
+    public List<String> transcribe(String name) throws 
+            SymbolSetNotExistException, IllegalSymbolException {
+        if (this.symbolSet == null) {
+            throw new SymbolSetNotExistException();
+        }
+        return this.symbolSet.transcribe(name);
+    }
+}
+
