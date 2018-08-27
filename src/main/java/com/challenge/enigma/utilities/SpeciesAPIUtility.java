ea0fa58cc3f671fd67328caef1520fd283f652f9
jexiaprogrammingchallenge <42712417+jexiaprogrammingchallenge@users.noreply.github.com>
2018-08-27 20:26:15 +0300

    stapi consumer utility is being developed.

--- src/main/java/com/challenge/enigma/utilities/SpeciesAPIUtility.java
+++ src/main/java/com/challenge/enigma/utilities/SpeciesAPIUtility.java
@@ -1,6 +1,9 @@
 package com.challenge.enigma.utilities;
 
+import com.challenge.enigma.models.Character;
 import com.challenge.enigma.models.CharacterList;
+import com.challenge.enigma.models.CharacterRoot;
+import com.challenge.enigma.models.CharacterSpecies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -23,5 +26,39 @@ public class SpeciesAPIUtility {
         HttpEntity<String> request = new HttpEntity<>(requestParameters, headers);
         characterList = rest.postForObject(URL + "/search", request, CharacterList.class);
         return characterList;
+    }
+
+    public String getSpecies(CharacterList characterList) {
+
+        String species = "";
+        boolean isSpeciesExist = false;
+        Character character;
+        
+        for (Character characterBuffer : characterList.getCharacters()) {
+            character = getCharacterByUID(characterBuffer.getUid());
+            for (CharacterSpecies characterSpecies : 
+                    character.getCharacterSpecies()) {
+                if (characterSpecies.getName() != null
+                        && !characterSpecies.getName().isEmpty()) {
+                    species = characterSpecies.getName();
+                    isSpeciesExist = true;
+                    break;
+                }
+            }
+            if (isSpeciesExist) {
+                break;
+            }
+        }
+        return species;
+    }
+
+    private Character getCharacterByUID(String uid) {
+        String uidRequest = "uid=" + uid;
+        CharacterRoot characterRoot = this.rest.getForObject(this.URL + "?"
+                + uidRequest, CharacterRoot.class);
+        if (characterRoot != null) {
+            return characterRoot.getCharacter();
+        }
+        return new Character();
     }
 }
